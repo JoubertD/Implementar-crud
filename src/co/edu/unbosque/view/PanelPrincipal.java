9a2c80873ee9118bed3eaa5849410c40c2282e2f
Rudexx <65428804+Rudexx@users.noreply.github.com>
2022-09-07 19:59:37 -0500

    algo de interfaz

--- src/co/edu/unbosque/view/PanelPrincipal.java
+++ src/co/edu/unbosque/view/PanelPrincipal.java
@@ -1,7 +1,74 @@
 package co.edu.unbosque.view;
 
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class PanelPrincipal extends JFrame {
+	private JLabel fondo;
+	private JTextField nickname;
+	private JButton continuar, salir;
+	
+	
+	/**
+	 * Constructor del PanelRegistro. Inicializa el panel
+	 */
 
+	public PanelPrincipal() {
+		setSize(1000, 700);
+		setBackground(Color.gray);
+		setLayout(null);
+		setVisible(false);
+		iniciarComponentes();
+	}
+	
+	/**
+	 * inicializa los componentes del panel
+	 * <b>pre</b> se ha inicializado el panel<br>
+	 * <b>post</b> se inicializaron los componentes del panel correctamente <br>
+	 * 
+	 */
+	
+	
+
+	public void iniciarComponentes() {
+		
+		
+
+		nickname = new JTextField();
+		nickname.setFont(new Font("Arial", Font.ROMAN_BASELINE, 12));
+		nickname.setBounds(423, 350, 200, 35);
+		nickname.setFont(new Font("Agency FB", Font.ITALIC, 18));
+		add(nickname);
+
+		salir = new JButton();
+		salir.setBounds(965, 5, 30, 30);
+		salir.setBorderPainted(false);
+		salir.setContentAreaFilled(false);
+		salir.setFocusPainted(true);
+		salir.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		salir.setActionCommand("SALIR8");
+		add(salir);
+
+		continuar = new JButton();	
+		continuar.setBounds(470, 400, 100, 50);
+		continuar.setBorderPainted(false);
+		continuar.setContentAreaFilled(false);
+		continuar.setFocusPainted(true);
+		continuar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		continuar.setActionCommand("REGISTRAR");
+		add(continuar);
+
+
+		fondo = new JLabel();
+		fondo.setSize(1000, 700);
+		fondo.setVisible(true);
+		add(fondo);
+
+	}
 }
--- src/co/edu/unbosque/view/VentanaPrincipal.java
+++ src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -1,9 +1,75 @@
 package co.edu.unbosque.view;
 
+import java.awt.Color;
+import java.awt.Cursor;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class VentanaPrincipal extends JFrame {
 
 	
+	private PanelPrincipal panelP;
+	private JButton iniciar, salir;
+	private JLabel intro;
+	
+	
+	public VentanaPrincipal() {
+
+		setTitle("Proyecto DAO");
+		setSize(1000, 700);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(new Color(244, 196, 196));
+		setResizable(false);
+		setLayout(null);
+		setUndecorated(true);
+		setLocationRelativeTo(null);
+		iniciarComponentes();
+
+		setVisible(true);
+
+	}
+	
+	/**
+	 * inicializa los componentes de la ventana
+	 * <b>pre</b> se ha inicializado la ventana<br>
+	 * <b>post</b> se inicializaron los componentes de la ventana correctamente <br>
+	 * 
+	 */
+	
+	
+
+	@SuppressWarnings("deprecation")
+	public void iniciarComponentes() {
+
+		panelP = new PanelPrincipal();
+		getContentPane().add(panelP);
+
+		iniciar = new JButton();
+		
+		iniciar.setBounds(360, 530, 100, 50);
+		iniciar.setBorderPainted(false);
+		iniciar.setContentAreaFilled(true);
+		iniciar.setFocusPainted(true);
+		iniciar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		iniciar.setActionCommand("INICIAR");
+		getContentPane().add(iniciar);
+
+		salir = new JButton();
+		salir.setBounds(510, 530, 100, 50);
+		salir.setBorderPainted(false);
+		salir.setContentAreaFilled(true);
+		salir.setFocusPainted(true);
+		salir.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		salir.setActionCommand("SALIR");
+		getContentPane().add(salir);
+		
+	
+
+		
+
+	}
+
 	
 }
